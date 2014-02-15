9480ab78cb928a53db4ae645af5321ea0689db38
Ashley Weaver <aweaver2012@gmail.com>
2014-02-15 16:09:11 -0500

    Added Bootstrap

--- brasite.py
+++ brasite.py
@@ -1,10 +1,12 @@
-from flask import Flask, flash
+from flask import Flask, flash, redirect, render_template, request, url_for
 from pymongo import MongoClient
-from secrets import MONGO_USER, MONGO_PASS, MONGO_URL, MONGO_PORT
-from wtforms import Form, TextField, validators
+from secrets import MONGO_USER, MONGO_PASS, MONGO_URL, MONGO_PORT, SECRET_KEY
+from wtforms import Form, TextField, BooleanField, validators
+import re
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.secret_key = SECRET_KEY
 
 client = MongoClient(MONGO_URL, MONGO_PORT)
 client.BraSniper.authenticate(MONGO_USER, MONGO_PASS)
@@ -12,7 +14,7 @@ db = client.BraSniper
 
 class RegistrationForm(Form):
 	email = TextField('Email Address', [validators.Length(min=6, max=35)])
-	sizes = TextField('Bra Sizes, separated by commas')
+	sizes = TextField('Bra Sizes, separated by spaces (no punctuation)')
 
 class UnregistrationForm(Form):
 	email = TextField('Email Address', [validators.Length(min=6, max=35)])
@@ -22,20 +24,24 @@ class UnregistrationForm(Form):
 def subscribe():
 	form = RegistrationForm(request.form)
 	if request.method == 'POST' and form.validate():
-		delimiters = " ", ","
-	 	regexPattern = '|'.join(map(re.escape, delimiters))
-		userSizes = re.split(regexPattern, title) 
-        db.users.insert({email: form.email.data, sizes: userSizes, sent: []}
-        flash('Thanks for subscribing!')
-    return render_template('subscribe.html', form=form)
+		if db.users.find({'email': form.email.data}):
+			flash('You have already subscribed!')
+			flash('Unsubscribe first if you want to update your sizes.')
+			return redirect(url_for('unsubscribe'))
+		userSizes = form.sizes.data.split()
+		db.users.insert({'email': form.email.data, 'sizes': userSizes, 'sent': []})
+		flash('Thanks for subscribing!')
+		return redirect(url_for('subscribe'))
+	return render_template('subscribe.html', form=form)
 
 @app.route('/unsubscribe', methods=['GET', 'POST'])
 def unsubscribe():
 	form = UnregistrationForm(request.form)
 	if request.method == 'POST' and form.validate():
-		db.users.remove({email: form.email.data})
+		db.users.remove({'email': form.email.data})
 		flash('You have successfully unsubscribed.')
+		return redirect(url_for('subscribe'))
 	return render_template('unsubscribe.html', form=form)
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug = True)
